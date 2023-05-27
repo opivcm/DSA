6bf7b24dcc3998e51de79e706e96fd5cd02f5bf9
OPIVCM <opivcm441@gmail.com>
2023-05-27 21:38:26 +0530

    seee

--- ARRAYS/Selectionsort.cpp
+++ ARRAYS/Selectionsort.cpp
@@ -1,5 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
+void selesort(int arr[],int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        int mid=i;
+
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[mid])
+            {
+                mid=j;
+            }
+        }
+        swap(arr[mid],arr[i]);
+    }
+}
 int main()
 {
     
