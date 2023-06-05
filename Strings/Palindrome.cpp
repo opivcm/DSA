d7c21dd53c4cca274b8c7f5124addbbfc9e0f1e8
OPIVCM <opivcm441@gmail.com>
2023-06-05 22:28:32 +0530

    Palindrome

--- Strings/Palindrome.cpp
+++ Strings/Palindrome.cpp
@@ -1,2 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
+bool palindrome(char a[], int n)
+{
+    int s=0;
+    int e=n-1;
+    while(s<e)
+    {
+        if(a[s]!=a[e])
+        {
+            return 0;
+        }
+        else
+        {
+            s++;
+            e--;
+        }
+    }
+    return 1;
+}
+int main()
+{
+    char name[20];
+    cout<<"Enter a word"<<endl;
+    cin>>name;
+    
+}
