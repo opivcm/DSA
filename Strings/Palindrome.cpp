ff43adbd72d5091eb3c82adbb8c8677b36779f80
OPIVCM <opivcm441@gmail.com>
2023-06-05 22:31:10 +0530

    PALINDRONE

--- Strings/Palindrome.cpp
+++ Strings/Palindrome.cpp
@@ -18,10 +18,23 @@ bool palindrome(char a[], int n)
     }
     return 1;
 }
+int length(char name[])
+{
+    int count=0;
+    for(int i=0;name[i]!=0;i++)
+    {
+        count++;
+    }
+    return count;
+}
 int main()
 {
     char name[20];
     cout<<"Enter a word"<<endl;
     cin>>name;
+    int len=length(name);
+    cout<<"lenght of your name is "<<len<<endl;
+    cout<<"palindrone or not"<<palindrome(name,len);
+    return 0;
     
 }
