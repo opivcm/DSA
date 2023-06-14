3fc5fe24b8a9b0b1906369a6ae0c27f8ff16b7ec
OPIVCM <opivcm441@gmail.com>
2023-06-14 16:17:36 +0530

    basic POINTERS

--- POINTERS/pointer.cpp
+++ POINTERS/pointer.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 int main()
 {
-    
+    int num=1;
+    cout<<num<<endl;
+    cout<<"address of num is "<<&num<<endl;
+    int *ptr= &num;
+    cout<<"address is: "<<ptr<<endl;
+    cout<<"value is: "<<*ptr<<endl;//value bascially double double is value.
+
+    cout<<"size of num is "<<sizeof(num)<<endl;
+    cout<<"size of pointer is "<<sizeof(ptr)<<endl;
+    return 0;
 }
