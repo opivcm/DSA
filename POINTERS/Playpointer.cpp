1f6b4adfebd204de948b419244c1fdd65203f946
OPIVCM <opivcm441@gmail.com>
2023-06-18 21:02:58 +0530

    playing with pointersss

---
+++ POINTERS/Playpointer.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[10]={4,5,6,7,41,65,9};
+    cout<<arr<<endl;//address
+    cout<<*arr<<endl;//value
+    cout<<arr[0]<<endl;//value
+    cout<<&arr[0]<<endl;//address
+}
