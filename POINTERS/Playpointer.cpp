ce65f4bf3bb1ba2378007143b302ad1a4627fb3a
OPIVCM <opivcm441@gmail.com>
2023-06-18 21:40:21 +0530

    PLAYING WITH ARRAYS

--- POINTERS/Playpointer.cpp
+++ POINTERS/Playpointer.cpp
@@ -3,9 +3,15 @@ using namespace std;
 
 int main()
 {
-    int arr[10]={4,5,6,7,41,65,9};
-    cout<<arr<<endl;//address
-    cout<<*arr<<endl;//value
+    int arr[10]={3,9,6,7,41,65,91};
+    cout<<arr<<endl;//address                               JUST REMEMBER 
+    cout<<*arr<<endl;//value                                THESE TWO aste.........
     cout<<arr[0]<<endl;//value
     cout<<&arr[0]<<endl;//address
+    cout<<*arr+1<<endl;//value of 0th Index +1
+    cout<<*(arr+1)<<endl;//value of next index
+    cout<<*(arr)+1<<endl;//same as *arr+1 value of 0 th index  + 1;
+    cout<<*(arr +2)<<endl;//VALUE 6 kodbeku.
+    cout<<arr[2]<<endl;//value 6 kodutte
+    cout<<2[arr]<<endl;
 }
