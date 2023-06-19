94801fa94da00e512ca8b2f4fafcdc212aeda444
OPIVCM <opivcm441@gmail.com>
2023-06-19 23:22:27 +0530

    playing with pointers

--- POINTERS/Playpointer.cpp
+++ POINTERS/Playpointer.cpp
@@ -15,6 +15,7 @@ int main()
     cout<<arr[2]<<endl;//value 6 kodutte                    arr[i]=*[arr+i]
     cout<<2[arr]<<endl;//wtf                                i[arr]=*[i+arr]
     cout<<2[arr+i]<<endl;
+    cout<<3[arr]<<endl;
 
 
     int temp[10];
