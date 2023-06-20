ca257cd8102d8edb8f14c974e16c7cbc90fa1d0c
OPIVCM <opivcm441@gmail.com>
2023-06-20 12:18:50 +0530

    PLAYING with pointers

--- POINTERS/POINTERSarrays.cpp
+++ POINTERS/POINTERSarrays.cpp
@@ -2,9 +2,15 @@
 using namespace std;
 int main()
 {
-int arr[20]={1,2,3,4,5,65,6,7,8};
-cout<<&arr[0]<<endl;
-cout<<&arr<<endl;
-cout<<arr<<endl;
+int arr[20]={99,2,3,4,5,65,6,7,8};
+cout<<&arr[0]<<endl;                    //address
+cout<<&arr<<endl;                       //address
+cout<<arr<<endl;                        //address
+
+int *p= &arr[0];
+cout<<p<<endl;                          //bari p is address
+cout<<*p<<endl;                         //* is VALUE
+cout<<&p<<endl;                         //& so it is address but ->>>>>>NEW ADDRESS<<<<<-
+
 return 0;
 }
