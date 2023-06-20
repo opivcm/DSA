236d71efc7d7ab08d2e92b0f0b43f9c6ee4ae80b
OPIVCM <opivcm441@gmail.com>
2023-06-20 12:23:18 +0530

    playing with pointers.

--- POINTERS/POINTERSarrays.cpp
+++ POINTERS/POINTERSarrays.cpp
@@ -2,15 +2,25 @@
 using namespace std;
 int main()
 {
-int arr[20]={99,2,3,4,5,65,6,7,8};
-cout<<&arr[0]<<endl;                    //address
-cout<<&arr<<endl;                       //address
-cout<<arr<<endl;                        //address
+//int arr[20]={99,2,3,4,5,65,6,7,8};
+//cout<<&arr[0]<<endl;                    //address
+//cout<<&arr<<endl;                       //address
+//cout<<arr<<endl;                        //address
 
-int *p= &arr[0];
-cout<<p<<endl;                          //bari p is address
-cout<<*p<<endl;                         //* is VALUE
-cout<<&p<<endl;                         //& so it is address but ->>>>>>NEW ADDRESS<<<<<-
+//int *p= &arr[0];
+//cout<<p<<endl;                          //bari p is address
+//cout<<*p<<endl;                         //* is VALUE
+//cout<<&p<<endl;                         //& so it is address but ->>>>>>NEW ADDRESS<<<<<-
 
+
+
+int arr[10]={1,2,3,45,5,5};
+//arr=arr+1;  //not possible symbol table mapping not possible 
+
+int *ptr=&arr[0];
+ptr=ptr+1;
+cout<<ptr<<endl;                        //symbol table cant change content
+ptr+=1;
+cout<<ptr<<endl;                        //same as type 1 
 return 0;
 }
