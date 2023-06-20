eacba2713ff8cbd44225a99caca80cc2c8ed3fe9
OPIVCM <opivcm441@gmail.com>
2023-06-20 12:31:09 +0530

    CHARACTER AND ARRAY

---
+++ POINTERS/characters.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[10]={1,2,3,4,5,9};
+    char ch[6]="vinay";
+
+    cout<<arr<<endl;            //prints ADDRESS
+    cout<<ch<<endl;             //prints WHOLE CHARACTER
+    return 0;
+}
