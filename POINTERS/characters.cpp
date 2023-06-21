248aa522f690a5c585fb95286f628a87a3d33ecc
OPIVCM <opivcm441@gmail.com>
2023-06-21 23:16:06 +0530

    Working with pointers and arrays

--- POINTERS/characters.cpp
+++ POINTERS/characters.cpp
@@ -7,5 +7,14 @@ int main()
 
     cout<<arr<<endl;            //prints ADDRESS
     cout<<ch<<endl;             //prints WHOLE CHARACTER
+
+    char *c=&ch[0]; 
+    cout<<c<<endl;              //prints WHOLE CHARACTER
+    //cout<<*c<<endl;             //first Character
+    //cout<<&c<<endl;             //address where it is stored & address
+
+    char temp='z';
+    char *p=&temp;
+    cout<<p<<endl;              
     return 0;
 }
