3a8b9434d58cbb78eadc9b34eb6a48757a0e14a8
OPIVCM <opivcm441@gmail.com>
2023-06-23 16:04:02 +0530

    pointer playing with functions

---
+++ POINTERS/poi_fubct.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int *p)
+{
+    cout<<p<<endl;   //address
+    cout<<*p<<endl;  //value
+}
+void update(int *p)
+{
+    //p=p+1;           //address update
+    //cout<<"inside the loop the right side p is incremented :-"<<p<<endl;
+    *p=*p+1;        //value update
+    cout<<"value of new value is  :-"<<*p<<endl;
+}
+int main()
+{
+    int value=5;
+    int *p=&value;
+    print(p);
+
+    cout<<"before update "<<p<<endl;  //same
+    cout<<"before update "<<*p<<endl;  //same
+    update(p);
+    cout<<"after update  "<<*p<<endl;  //same
+    return 0;
+}
