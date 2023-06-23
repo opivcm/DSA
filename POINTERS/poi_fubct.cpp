b144a32a26937445d51a3594f74fba8e87ec41d5
OPIVCM <opivcm441@gmail.com>
2023-06-23 17:12:27 +0530

    pointers played with functions

--- POINTERS/poi_fubct.cpp
+++ POINTERS/poi_fubct.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
 void print(int *p)
@@ -6,6 +6,12 @@ void print(int *p)
     cout<<p<<endl;   //address
     cout<<*p<<endl;  //value
 }
+
+int getsum(int arr[],int *p)
+{
+
+}
+
 void update(int *p)
 {
     //p=p+1;           //address update
@@ -13,15 +19,18 @@ void update(int *p)
     *p=*p+1;        //value update
     cout<<"value of new value is  :-"<<*p<<endl;
 }
+
 int main()
 {
+    int arr[10];
     int value=5;
     int *p=&value;
     print(p);
 
-    cout<<"before update "<<p<<endl;  //same
-    cout<<"before update "<<*p<<endl;  //same
-    update(p);
-    cout<<"after update  "<<*p<<endl;  //same
+    //cout<<"before update "<<p<<endl; 
+   //cout<<"before update "<<*p<<endl;  //prints *p value
+   // update(p);
+   // cout<<"after update  "<<*p<<endl;  //*p is being update with new value 6
+   cout<<getsum(arr)<<endl;
     return 0;
 }
